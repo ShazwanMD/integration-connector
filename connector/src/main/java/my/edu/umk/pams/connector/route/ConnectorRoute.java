f3e4d51b67acbee661d20ce9de7fc034b07e41f7
Shazwan MD <shazwan.md@umk.edu.my>
2017-08-30 15:40:06 +0800

    update integration Connector

--- connector/src/main/java/my/edu/umk/pams/connector/route/ConnectorRoute.java
+++ connector/src/main/java/my/edu/umk/pams/connector/route/ConnectorRoute.java
@@ -24,60 +24,88 @@ import my.edu.umk.pams.connector.processor.CandidateQueueSyncProcessor;
 @Component
 public class ConnectorRoute extends RouteBuilder {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ConnectorRoute.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ConnectorRoute.class);
 
-    @Autowired
-    private ConnectionFactory connectionFactory;
+	@Autowired
+	private ConnectionFactory connectionFactory;
 
-    @PostConstruct
-    public void postConstruct() {
-        LOG.info("Loading ConnectorRoute");
-    }
+	@PostConstruct
+	public void postConstruct() {
+		LOG.info("Loading ConnectorRoute");
+	}
 
-    @Override
-    public void configure() throws Exception {
-        JmsComponent component = new JmsComponent();
-        component.setConnectionFactory(connectionFactory);
-        getContext().addComponent("jms", component);
+	@Override
+	public void configure() throws Exception {
+		JmsComponent component = new JmsComponent();
+		component.setConnectionFactory(connectionFactory);
+		getContext().addComponent("jms", component);
 
-        from("jms:queue:candidateQueue")
-                .routeId("candidateQueueRoute")
-                .log("incoming candidate")
-                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .to("http4://{{rest.academic.host}}:{{rest.academic.port}}/api/integration/candidates")
-                .end();
+		from("jms:queue:candidateQueue").routeId("candidateQueueRoute").log("incoming candidate")
+				.setHeader(Exchange.HTTP_METHOD, constant("POST"))
+				.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+				.to("http4://{{rest.academic.host}}:{{rest.academic.port}}/api/integration/candidates").end();
 
-        from("jms:queue:programCodeQueue")
-                .routeId("programCodeQueue")
-                .log("incoming program code")
-                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .to("http4://{{rest.account.host}}:{{rest.account.port}}/api/integration/programCodes")
-                .end();
+		from("jms:queue:programCodeQueue").routeId("programCodeQueue").log("incoming program code")
+				.setHeader(Exchange.HTTP_METHOD, constant("POST"))
+				.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+				.to("http4://{{rest.account.host}}:{{rest.account.port}}/api/integration/programCodes").end();
 
-        from("jms:queue:facultyCodeQueue")
-                .routeId("facultyCodeQueue")
-                .log("incoming faculty code")
-                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .to("http4://{{rest.account.host}}:{{rest.account.port}}/api/integration/facultyCodes")
-                .end();
-        from("jms:topic:facultyCodeTopic")
-		        .routeId("facultyCodeTopic")
-		        .log("incoming faculty code")
-		        .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-		        .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-		        .to("http4://{{rest.account.host}}:{{rest.account.port}}/api/integration/facultyCodes")
-		        .to("http4://{{rest.intake.host}}:{{rest.intake.port}}/api/integration/facultyCodes")
-		        .end();
-        
-        from("jms:queue:accountQueue")
-		        .routeId("accountQueue")
-		        .log("incoming Account Student")
-		        .setHeader(Exchange.HTTP_METHOD, constant("PUT"))
-		        .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-		        .to("http4://{{rest.academic.host}}:{{rest.academic.port}}/api/integration/studentAccounts")
-		        .end();
-    }
+		from("jms:queue:facultyCodeQueue2")
+		.routeId("facultyCodeQueue2")
+		.log("Incoming Faculty Code")
+		.multicast()
+		.to("direct:intake","direct:account")
+		.end();
+
+		from("direct:intake")
+		.log("intake faculty code")
+		.setHeader(Exchange.HTTP_METHOD, constant("POST"))
+		.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+		.to("http4://{{rest.intake.host}}:{{rest.intake.port}}/api/integration/facultyCodes").end();
+
+		from("direct:account")
+		.log("account faculty code")
+		.setHeader(Exchange.HTTP_METHOD, constant("POST"))
+		.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+		.to("http4://{{rest.account.host}}:{{rest.account.port}}/api/integration/facultyCodes")
+		.end();
+
+		// from("jms:queue:facultyCodeQueue")
+		// .routeId("facultyCodeQueue")
+		// .log("incoming faculty code")
+		// .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+		// .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+		// .to("http4://{{rest.account.host}}:{{rest.account.port}}/api/integration/facultyCodes")
+		// .end();
+		// from("direct:start").to("activemq:topic:VirtualTopic.foo");
+		//  
+		// from("activemq:queue:Consumer.1.VirtualTopic.foo").to("mock:result1");
+		//  
+		// from("activemq:queue:Consumer.2.VirtualTopic.foo").to("mock:result2");
+
+		// from("jms:topic:facultyCodeTopic")
+		// .routeId("facultyCodeTopic")
+		// .log("incoming faculty code")
+		// .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+		// .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+		// .to("activemq:topic:facultyCodeTopic")
+		// .end();
+
+		// from("jms:queue:acfacultyCodeQueue").routeId("acFacultyCodeQueue").log("incoming
+		// faculty code")
+		// .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+		// .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+		// .to("http4://{{rest.account.host}}:{{rest.account.port}}/api/integration/facultyCodes").end();
+		//
+		// from("jms:queue:inFacultyCodeQueue").routeId("inFacultyCodeQueue").log("incoming
+		// faculty code")
+		// .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+		// .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+		// .to("http4://{{rest.intake.host}}:{{rest.intake.port}}/api/integration/facultyCodes").end();
+
+		from("jms:queue:accountQueue").routeId("accountQueue").log("incoming Account Student")
+				.setHeader(Exchange.HTTP_METHOD, constant("PUT"))
+				.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+				.to("http4://{{rest.academic.host}}:{{rest.academic.port}}/api/integration/studentAccounts").end();
+	}
 }
